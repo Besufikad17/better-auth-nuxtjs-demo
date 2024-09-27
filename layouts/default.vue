2995716608a0077c7e239b983034150c1980046f
Besufikad <besumicheal@gmail.com>
2024-09-27 15:02:06 +0500

    fix: dark mode and responsivness issues fixed

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,15 +6,15 @@
 <template>
     <div>
         <div class="flex items-center justify-center dark:bg-black">
-            <div class="flex items-center justify-between border-b-2 border-gray-300 p-2 w-full lg:w-2/3">
+            <div class="flex items-center justify-between border-b border-gray-300 p-2 w-full lg:w-2/3">
                 <div class="flex items-center gap-3">
                     <span class="flex items-center justify-center p-2 bg-gray-200 text-xs">B.</span>
-                    <span class="dark:text-gray-200">BETTER AUTH.</span>
+                    <span class="dark:text-gray-200">BETTER-AUTH.</span>
                 </div>
                 <client-only>
-                    <div class="dark:text-gray-200">
-                        <Icon v-if="mode === 'dark'" name="lucide:sun" @click="mode = 'light'" />
-                        <Icon v-else name="lucide:moon" @click="mode = 'dark'" />
+                    <div class="dark:text-gray-200 hover:bg-gray-300 cursor-pointer dark:hover:bg-primary-800 p-2 rounded-sm flex items-center justify-center">
+                        <Icon v-if="mode === 'light'" name="lucide:sun" @click="mode = 'dark'" />
+                        <Icon v-else name="lucide:moon" @click="mode = 'light'" />
                     </div>
                 </client-only>
             </div>
--- pages/index.vue
+++ pages/index.vue
@@ -12,16 +12,18 @@
 </script>
 
 <template>
-    <div class="flex flex-col items-center justify-center gap-6 min-h-screen w-full dark:bg-black bg-white dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2] overflow-hidden">
-        <h1 class="dark:text-gray-200 text-4xl">Better Auth.</h1>
-        <span class="dark:text-gray-200 text-xl">Nuxt.js demo to showcase  <a href="https://better-auth.com/" class="underline italic" target="_blank">better-auth.</a> features and capabilities.</span>
-        <span class="flex items-center justify-center bg-gray-200 dark:bg-gray-300 opacity-80 p-2 text-xs text-center">
+    <div class="flex flex-col items-center justify-center gap-6 px-4 h-screen w-full dark:bg-black bg-white 
+      bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:12px_12px] overflow-hidden
+      dark:bg-[linear-gradient(to_right,#ffffff12_1px,transparent_1px),linear-gradient(to_bottom,#ffffff12_1px,transparent_1px)]">
+        <h1 class="dark:text-gray-200 text-4xl font-semibold">Better Auth.</h1>
+        <span class="dark:text-gray-200 text-md md:text-xl text-center">Nuxt.js demo to showcase  <a href="https://better-auth.com/" class="underline italic" target="_blank">better-auth.</a> features and capabilities.</span>
+        <span class="flex items-center justify-center bg-[rgba(229,231,235,0.5)] dark:text-gray-400 dark:bg-[rgba(84,77,74,0.4)] p-2 text-xs text-center">
             All features on this demo are Implemented with better auth without any custom backend code
         </span>
         <div class="flex flex-wrap justify-center gap-2 md:w-1/2">
             <span v-for="(feature, index) in FEATURES" :key="index" class="text-xs text-gray-500 hover:text-gray-800 dark:hover:text-gray-100 hover:underline cursor-pointer">{{ feature }}.</span>
         </div>
-        <button class="bg-primary-950 hover:bg-primary-900 flex items-center gap-2 text-gray-100 px-4 py-2 text-sm">
+        <button class="bg-primary-950 dark:bg-white dark:text-black hover:bg-primary-900 flex items-center gap-2 text-gray-100 px-4 py-2 text-sm">
             <Icon name="lucide:log-in" />
             Sign In
         </button>
