0ab126406399affe856786a9eef69059cf90c47c
bes <besufikadtilahun1994@gmail.com>
2024-10-26 08:48:40 +0300

    mod: client plugins import updated

--- composables/functions/client.ts
+++ composables/functions/client.ts
@@ -1,6 +1,5 @@
 import { createAuthClient } from "better-auth/vue";
-import { passkeyClient, twoFactorClient } from "better-auth/plugins"; 
-import { multiSessionClient, organizationClient } from "better-auth/client/plugins"
+import { passkeyClient, twoFactorClient, multiSessionClient, organizationClient } from "better-auth/client/plugins"
 
 export const client = createAuthClient({
     baseURL: process.env.BETTER_AUTH_URL,
--- server/api/sessions.ts
+++
@@ -1,6 +0,0 @@
-import { auth } from "~/composables/functions/auth";
-
-export default defineEventHandler(async(event) => {
-    const accts = await auth.api.listDeviceSessions({ headers: new Headers() });
-    return accts;
-});
