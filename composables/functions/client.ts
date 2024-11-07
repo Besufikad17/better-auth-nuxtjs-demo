6e1f0531cb1e658641cdc1b8813477d29a682a3a
bes <besufikadtilahun1994@gmail.com>
2024-11-07 20:04:01 +0300

    fix: baseURL bug fixed

--- composables/functions/client.ts
+++ composables/functions/client.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/vue";
 import { passkeyClient, twoFactorClient, multiSessionClient, organizationClient } from "better-auth/client/plugins"
 
 export const client = createAuthClient({
-    baseURL: process.env.BETTER_AUTH_URL,
+    baseURL: getBaseURL(),
     plugins: [
         multiSessionClient(),
         organizationClient(),
@@ -12,3 +12,14 @@ export const client = createAuthClient({
         })
     ]
 });
+
+function getBaseURL() {
+    let baseURL = process.env.BETTER_AUTH_URL
+    if (!baseURL) {
+      try {
+        baseURL = getRequestURL(useEvent()).origin
+      }
+      catch (e) {}
+    }
+    return baseURL
+}
